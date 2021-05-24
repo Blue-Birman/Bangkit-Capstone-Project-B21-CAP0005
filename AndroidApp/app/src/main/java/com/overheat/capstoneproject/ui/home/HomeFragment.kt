2e032627d4cb61547938799b016c766dff4305f2
Alexander Tri Handoyo Wangsaputra <ATHWangsaputra@gmail.com>
2021-05-24 15:28:12 +0700

    Finish home fragment layout
    Using recyclerview and carousel library to finish the layout

--- AndroidApp/app/src/main/java/com/overheat/capstoneproject/ui/home/HomeFragment.kt
+++ AndroidApp/app/src/main/java/com/overheat/capstoneproject/ui/home/HomeFragment.kt
@@ -1,31 +1,77 @@
 package com.overheat.capstoneproject.ui.home
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.TextView
+import android.view.*
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.bumptech.glide.Glide
 import com.overheat.capstoneproject.R
+import com.overheat.capstoneproject.databinding.FragmentHomeBinding
+import com.synnapps.carouselview.ImageListener
 
 class HomeFragment : Fragment() {
 
-    private lateinit var homeViewModel: HomeViewModel
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
+    ): View {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        fillHomeFragment()
+        prepareRecyclerView()
+        prepareCarouselView()
+    }
+
+    private fun fillHomeFragment() {
+        with(binding) {
+            tvWelcomeUser.text = context?.resources?.getString(R.string.welcome_guest, "Guest")
+            tvWelcomeText.text = "lorem ipsum dolor sit amet"
+        }
+    }
+
+    private fun prepareRecyclerView() {
+        val adapter = HomeListFaqAdapter()
+
+        with(binding.rvFaqs) {
+            this.layoutManager = LinearLayoutManager(context)
+            this.adapter = adapter
+        }
+    }
+
+    private fun prepareCarouselView() {
+        val sampleImages = arrayOf(
+            "https://raw.githubusercontent.com/sayyam/carouselview/master/sample/src/main/res/drawable/image_3.jpg",
+            "https://raw.githubusercontent.com/sayyam/carouselview/master/sample/src/main/res/drawable/image_1.jpg",
+            "https://raw.githubusercontent.com/sayyam/carouselview/master/sample/src/main/res/drawable/image_2.jpg"
+        )
+
+        with(binding.carouselView) {
+            pageCount = sampleImages.size
+            setImageListener { position, imageView ->
+                Glide.with(this@HomeFragment)
+                    .load(sampleImages[position])
+                    .into(imageView)
+            }
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.notifications_menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
     }
 }
