72e3792450e987eea8b4ae7fab020fddc2517911
katherineryn1 <katherineryn1@gmail.com>
2021-05-28 16:37:49 +0700

    finished photo fragment

--- AndroidApp/app/src/main/java/com/overheat/capstoneproject/ui/photo/PhotoFragment.kt
+++ AndroidApp/app/src/main/java/com/overheat/capstoneproject/ui/photo/PhotoFragment.kt
@@ -1,15 +1,26 @@
 package com.overheat.capstoneproject.ui.photo
 
+import android.Manifest
+import android.app.Activity.RESULT_CANCELED
+import android.app.Activity.RESULT_OK
+import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.*
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import com.overheat.capstoneproject.R
 import com.overheat.capstoneproject.databinding.FragmentPhotoBinding
+
 
 class PhotoFragment : Fragment() {
 
     private var _binding: FragmentPhotoBinding? = null
     private val binding get() = _binding!!
+
+    val REQUEST_CODE = 200
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -17,6 +28,48 @@ class PhotoFragment : Fragment() {
     ): View {
         _binding = FragmentPhotoBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        checkPermission()
+
+        binding.btnCapture.setOnClickListener {
+            openCamera()
+        }
+    }
+
+    private fun checkPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(
+                requireActivity(),
+                Manifest.permission.CAMERA
+            )
+        ) {
+            Toast.makeText(requireContext(), "Permission allowed", Toast.LENGTH_SHORT).show();
+        } else {
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                arrayOf(Manifest.permission.CAMERA),
+                REQUEST_CODE
+            );
+        }
+    }
+
+    private fun openCamera() {
+        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        startActivityForResult(cameraIntent, REQUEST_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (resultCode != RESULT_CANCELED) {
+            when (requestCode) {
+                200 -> if (resultCode == RESULT_OK && data != null) {
+                    val selectedImage = data.extras?.get("data") as Bitmap
+                    binding.ivPhoto.setImageBitmap(selectedImage)
+                }
+            }
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
