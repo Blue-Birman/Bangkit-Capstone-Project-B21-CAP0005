42ec62555d8e6e3e493bd88b136aec35fd392e43
AlexTri12 <ATHWangsaputra@gmail.com>
2021-06-02 17:05:24 +0700

    Adding all repository to domain use case and interactor

--- AndroidApp/app/src/main/java/com/overheat/capstoneproject/core/domain/usecase/SkinCancerInteractor.kt
+++ AndroidApp/app/src/main/java/com/overheat/capstoneproject/core/domain/usecase/SkinCancerInteractor.kt
@@ -2,7 +2,10 @@ package com.overheat.capstoneproject.core.domain.usecase
 
 import com.overheat.capstoneproject.core.data.Resource
 import com.overheat.capstoneproject.core.domain.model.Article
+import com.overheat.capstoneproject.core.domain.model.DetailArticle
+import com.overheat.capstoneproject.core.domain.model.Diagnose
 import com.overheat.capstoneproject.core.domain.model.Faq
+import com.overheat.capstoneproject.core.domain.model.Result
 import com.overheat.capstoneproject.core.domain.repository.ISkinCancerRepository
 import kotlinx.coroutines.flow.Flow
 
@@ -16,5 +19,37 @@ class SkinCancerInteractor(
 
     override fun getAllArticles(): Flow<Resource<List<Article>>> {
         return repository.getAllArticles()
+    }
+
+    override fun getDetailArticle(articleId: Int): DetailArticle? {
+        return repository.getDetailArticle(articleId)
+    }
+
+    override suspend fun sendComment(articleId: Int, comment: String): Boolean {
+        return repository.sendComment(articleId, comment)
+    }
+
+    override fun getAllHistoryDiagnose(userId: Int): Flow<Resource<List<Diagnose>>> {
+        return repository.getAllHistoryDiagnose(userId)
+    }
+
+    override suspend fun getResultFromImage(image: String): Result? {
+        return getResultFromImage(image)
+    }
+
+    override suspend fun getDiagnoseResult(resultId: Int): Diagnose? {
+        return getDiagnoseResult(resultId)
+    }
+
+    override fun getActiveToken(email: String, passHash: String) {
+        return getActiveToken(email, passHash)
+    }
+
+    override fun addNewUser(name: String, email: String, passHash: String) {
+        return addNewUser(name, email, passHash)
+    }
+
+    override fun userLogout() {
+        return userLogout()
     }
 }
