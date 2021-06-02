95db8d2e081fe8612352fe94c7429ddda53f2de6
katherineryn1 <katherineryn1@gmail.com>
2021-06-02 18:01:00 +0700

    connect photo fragment to api

--- AndroidApp/app/src/main/java/com/overheat/capstoneproject/ui/photo/PhotoFragment.kt
+++ AndroidApp/app/src/main/java/com/overheat/capstoneproject/ui/photo/PhotoFragment.kt
@@ -8,20 +8,23 @@ import android.graphics.Bitmap
 import android.os.Bundle
 import android.provider.MediaStore
 import android.util.Base64
-import android.view.*
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.graphics.drawable.toBitmap
 import androidx.fragment.app.Fragment
-import com.overheat.capstoneproject.R
 import com.overheat.capstoneproject.databinding.FragmentPhotoBinding
-import com.overheat.capstoneproject.ui.news.DetailNewsViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import org.koin.android.viewmodel.ext.android.viewModel
 import java.io.ByteArrayOutputStream
 
 class PhotoFragment : Fragment() {
 
-    private val detailNewsViewModel: DetailNewsViewModel by viewModel()
+    private val photoViewModel: PhotoViewModel by viewModel()
 
     private var _binding: FragmentPhotoBinding? = null
     private val binding get() = _binding!!
@@ -46,7 +49,9 @@ class PhotoFragment : Fragment() {
         }
 
         binding.btnSend.setOnClickListener {
-            getResult()
+            GlobalScope.launch(Dispatchers.Main) {
+                getResult()
+            }
         }
 
         binding.popupResult.btnClose.setOnClickListener {
@@ -75,7 +80,7 @@ class PhotoFragment : Fragment() {
         startActivityForResult(cameraIntent, REQUEST_CODE)
     }
 
-    private fun getResult() {
+    private suspend fun getResult() {
         val imageString = getImage()
 
         // resize image
@@ -83,20 +88,33 @@ class PhotoFragment : Fragment() {
             Bitmap.createScaledBitmap((binding.ivPhoto.drawable).toBitmap(), 150, 150, true)
 
         // send the image string to api
-
+        val result = photoViewModel.getResult(imageString)
 
         // show result
-        showPopUp(bitmap, 0.7) // pass bitmap and cancer proba from api
+        showPopUp(bitmap, result?.cancerProba) // pass bitmap and cancer proba from api
     }
 
-    private fun showPopUp(bitmap: Bitmap, cancerProba: Double) {
+    private fun showPopUp(bitmap: Bitmap, cancerProba: Double?) {
         binding.popupResult.ivResult.setImageBitmap(bitmap)
-        binding.popupResult.tvResult.text = cancerProba.toString()
 
-        if (cancerProba > 0.5) {
-            binding.popupResult.tvWarning.text = "You should check your condition to doctor."
-        } else {
-            binding.popupResult.tvWarning.text = "..."
+        val cancerPercentage = cancerProba?.times(100)
+        binding.popupResult.tvResult.text = cancerPercentage.toString()
+
+        var warningText = ""
+        if (cancerProba != null) {
+            when {
+                cancerProba > 0.7 -> {
+                    warningText = "You should check your condition immediately."
+                }
+                cancerProba > 0.4 -> {
+                    warningText = "Not sure, but it's better if you check your condition"
+                }
+                else -> {
+                    warningText = "Congrats, you're okay"
+                }
+            }
+
+            binding.popupResult.tvWarning.text = warningText
         }
 
         binding.btnCapture.isEnabled = false
