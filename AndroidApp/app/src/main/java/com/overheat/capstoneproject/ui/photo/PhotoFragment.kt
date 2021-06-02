f79b240ac6a63232daa8c70b71881ce42f252ca0
katherineryn1 <katherineryn1@gmail.com>
2021-06-02 11:33:47 +0700

    change result activity into popup

--- AndroidApp/app/src/main/java/com/overheat/capstoneproject/ui/photo/PhotoFragment.kt
+++ AndroidApp/app/src/main/java/com/overheat/capstoneproject/ui/photo/PhotoFragment.kt
@@ -5,7 +5,6 @@ import android.app.Activity.RESULT_CANCELED
 import android.app.Activity.RESULT_OK
 import android.content.Intent
 import android.graphics.Bitmap
-import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.provider.MediaStore
 import android.util.Base64
@@ -16,16 +15,12 @@ import androidx.core.graphics.drawable.toBitmap
 import androidx.fragment.app.Fragment
 import com.overheat.capstoneproject.R
 import com.overheat.capstoneproject.databinding.FragmentPhotoBinding
-import com.overheat.capstoneproject.ui.result.ResultActivity
 import java.io.ByteArrayOutputStream
-import java.util.*
 
 class PhotoFragment : Fragment() {
 
     private var _binding: FragmentPhotoBinding? = null
     private val binding get() = _binding!!
-
-    val REQUEST_CODE = 200
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -39,13 +34,19 @@ class PhotoFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         checkPermission()
+        binding.btnSend.isEnabled = false
 
         binding.btnCapture.setOnClickListener {
             openCamera()
+            binding.btnSend.isEnabled = true
         }
 
         binding.btnSend.setOnClickListener {
-            sendToApi()
+            getResult()
+        }
+
+        binding.popupResult.btnClose.setOnClickListener {
+            closePopUp()
         }
     }
 
@@ -70,24 +71,48 @@ class PhotoFragment : Fragment() {
         startActivityForResult(cameraIntent, REQUEST_CODE)
     }
 
-    private fun sendToApi() {
+    private fun getResult() {
         val imageString = getImage()
-        val intent = Intent(context, ResultActivity::class.java).apply {
-            putExtra(ResultActivity.EXTRA_IMAGE, (binding.ivPhoto.drawable.toBitmap()))
-            putExtra(ResultActivity.EXTRA_STRING, imageString)
-        }
-        startActivity(intent)
+        // resize image
+        val bitmap =
+            Bitmap.createScaledBitmap((binding.ivPhoto.drawable).toBitmap(), 150, 150, true)
+
+        // send the image string to api
+
+        // show result
+        showPopUp(bitmap, 0.7)
     }
 
-    private fun getImage() : String {
+    private fun showPopUp(bitmap: Bitmap, cancerProba: Double) {
+        binding.popupResult.ivResult.setImageBitmap(bitmap)
+        binding.popupResult.tvResult.text = cancerProba.toString()
+
+        if (cancerProba > 0.5) {
+            binding.popupResult.tvWarning.text = "You should check your condition to doctor."
+        } else {
+            binding.popupResult.tvWarning.text = "..."
+        }
+
+        binding.btnCapture.isEnabled = false
+        binding.btnSend.isEnabled = false
+        binding.popupResult.root.bringToFront()
+        binding.popupResult.root.visibility = View.VISIBLE
+    }
+
+    private fun closePopUp() {
+        binding.btnCapture.isEnabled = true
+        binding.popupResult.root.visibility = View.GONE
+    }
+
+    private fun getImage(): String {
         val byteArrayOutputStream = ByteArrayOutputStream()
-        val bitmap = (binding.ivPhoto.drawable).toBitmap()
-        bitmap.compress(Bitmap.CompressFormat.JPEG, 150, byteArrayOutputStream)
+        val bitmap =
+            Bitmap.createScaledBitmap((binding.ivPhoto.drawable).toBitmap(), 150, 150, true)
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream)
 
         val imageBytes = byteArrayOutputStream.toByteArray()
-        val imageString = Base64.encodeToString(imageBytes, Base64.DEFAULT)
 
-        return imageString
+        return Base64.encodeToString(imageBytes, Base64.DEFAULT)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -109,5 +134,9 @@ class PhotoFragment : Fragment() {
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
+    }
+
+    companion object {
+        const val REQUEST_CODE = 200
     }
 }
