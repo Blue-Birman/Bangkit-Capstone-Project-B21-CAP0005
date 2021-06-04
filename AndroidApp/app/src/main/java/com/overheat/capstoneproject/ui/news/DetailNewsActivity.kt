6a81da7a8b5101d3a9490fc0341bfdcb1da1d0c0
AlexTri12 <ATHWangsaputra@gmail.com>
2021-06-04 18:13:39 +0700

    Fixing some UI problems

--- AndroidApp/app/src/main/java/com/overheat/capstoneproject/ui/news/DetailNewsActivity.kt
+++ AndroidApp/app/src/main/java/com/overheat/capstoneproject/ui/news/DetailNewsActivity.kt
@@ -1,5 +1,6 @@
 package com.overheat.capstoneproject.ui.news
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Base64
 import android.view.View
@@ -8,6 +9,7 @@ import com.bumptech.glide.Glide
 import com.overheat.capstoneproject.R
 import com.overheat.capstoneproject.core.domain.model.DetailArticle
 import com.overheat.capstoneproject.databinding.ActivityDetailNewsBinding
+import com.overheat.capstoneproject.ui.comments.CommentActivity
 import org.koin.android.viewmodel.ext.android.viewModel
 
 class DetailNewsActivity : AppCompatActivity() {
@@ -18,6 +20,9 @@ class DetailNewsActivity : AppCompatActivity() {
 
     private val viewModel: DetailNewsViewModel by viewModel()
     private lateinit var binding: ActivityDetailNewsBinding
+    private var isLiked = false
+    private var likes = 0
+    private var comments = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -52,6 +57,30 @@ class DetailNewsActivity : AppCompatActivity() {
 
             tvContent.text = detail.article.article
             collapsingToolbarLayout.title = detail.article.title
+
+            // Update likes and comments count
+            tvLike.text = resources.getString(R.string.text_likes, likes.toString())
+            tvComment.text = resources.getString(R.string.text_comments, comments.toString())
+
+            // On click listener for image view
+            imgLike.setOnClickListener {
+                isLiked = !isLiked
+                if (isLiked) {
+                    likes += 1
+                    tvLike.text = resources.getString(R.string.text_likes, likes.toString())
+                    tvLike.setTextColor(resources.getColor(R.color.blue))
+                } else {
+                    likes -= 1
+                    tvLike.text = resources.getString(R.string.text_likes, likes.toString())
+                    tvLike.setTextColor(resources.getColor(R.color.black))
+                }
+            }
+
+            imgComment.setOnClickListener {
+                val intent = Intent(this@DetailNewsActivity, CommentActivity::class.java)
+                startActivity(intent)
+            }
+            bottomNavigation.visibility = View.VISIBLE
         }
     }
 }
