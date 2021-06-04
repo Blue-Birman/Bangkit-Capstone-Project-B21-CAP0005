0e4827cff845f31c216794fb5e410ebc8b28216c
AlexTri12 <ATHWangsaputra@gmail.com>
2021-06-04 16:25:46 +0700

    Adding comment and profile layout

---
+++ AndroidApp/app/src/main/java/com/overheat/capstoneproject/ui/profile/ProfileViewModel.kt
@@ -0,0 +1,7 @@
+package com.overheat.capstoneproject.ui.profile
+
+import androidx.lifecycle.ViewModel
+import com.overheat.capstoneproject.core.domain.usecase.SkinCancerUseCase
+
+class ProfileViewModel(private val useCase: SkinCancerUseCase) : ViewModel() {
+}
